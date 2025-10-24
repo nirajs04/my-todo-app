e43d9ceda808047adeb85b9660eb7d1d92711394
Niraj Kumar Singh <nirajsingh041119@gmail.com>
2025-10-24 11:55:28 +0530

    Feat: Create TodoItem component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 // src/App.js
-import './App.css';
+import React, { useState } from 'react';
 
+// This is a separate component for a single todo item
+function TodoItem({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>x</button>
+      </div>
+    </div>
+  );
+}
 // This is the main App component
 function App() {
-  // We will add the list of todos here later
-  const todos = [
+  const [todos, setTodos] = useState([ // <-- ADDED THIS
     { text: 'Learn React', isCompleted: false },
     { text: 'Build portfolio project', isCompleted: false },
     { text: 'Apply for job', isCompleted: false },
-  ];
+  ]);
 
   return (
     <div className="app">
       <div className="todo-list">
         <h1>My To-Do List</h1>
-        {/* We will map over the todos here later */}
+        {todos.map((todo, index) => ( // <-- This line will now work
+          <TodoItem
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={() => {}} /* Placeholder function */
+            removeTodo={() => {}}   /* Placeholder function */
+          />
+        ))}
       </div>
     </div>
   );
